3a4feae064bd465c8ae27782534a00e7342a3ed9
yu.he <gannicus_yu@163.com>
2019-10-17 10:24:55 +0800

    getUserInfo

--- src/main/resources/public/src/api/login.js
+++ src/main/resources/public/src/api/login.js
@@ -13,7 +13,7 @@ export function login(username, password) {
 
 export function getInfo(token) {
   return request({
-    url: '/user/info',
+    url: '/user/api/info',
     method: 'get',
     params: { token }
   })
